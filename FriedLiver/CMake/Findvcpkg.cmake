9b99f95258dafd5f3de88394e0d0a227285bb49d
cyy <cyyever@outlook.com>
2018-12-04 17:38:21 +0800

    add cmake support

---
+++ FriedLiver/CMake/Findvcpkg.cmake
@@ -0,0 +1,19 @@
+# - Try to find vcpkg
+#
+# The following are set after configuration is done:
+#  vcpkg_FOUND
+#  vcpkg_dir
+#  vcpkg_toolchain_file
+
+include(FindPackageHandleStandardArgs)
+if(NOT DEFINED vcpkg_dir)
+  find_file(vcpkg_dir vcpkg PATHS ~)
+  find_package_handle_standard_args(vcpkg DEFAULT_MSG vcpkg_dir)
+  if(NOT vcpkg_FOUND)
+    return()
+  endif()
+else()
+  file(TO_CMAKE_PATH "${vcpkg_dir}" vcpkg_dir)
+endif()
+find_file(vcpkg_toolchain_file vcpkg.cmake PATHS ${vcpkg_dir}/scripts/buildsystems)
+find_package_handle_standard_args(vcpkg DEFAULT_MSG vcpkg_dir vcpkg_toolchain_file)
